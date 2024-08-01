673721d1e0d8bd33cabce13a3c9719bd41b3b518
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-08-01 18:15:36 +0200

    esercizio terminato

--- script.js
+++ script.js
@@ -1,56 +1,44 @@
-//Preparazione: check JS / richiamo elementi nel dom
+console.log('JS OK');
 
-console.log('JS OK')
-const nameInput = document.querySelector ('#name');
-const kmsInput = document.querySelector ('#kms');
-const ageInput = document.querySelector ('#age');
-const button = document.querySelector ('button');
-const paragraphElement = document.querySelector ('p');
+const nameInput = document.querySelector('#name');
+const kmsInput = document.querySelector('#kms');
+const ageSelect = document.querySelector('#age');
 const form = document.querySelector('#ticket');
-
-console.log('name', nameInput)
-console.log('kms', kmsInput)
-console.log('age', ageInput)
+const paragraphElement = document.querySelector('#paragraph');
 
 const pricePerKm = 0.21;
-let discount = null ;
 
-//Gestione Eventi: recupero valore input
-form.addEventListener('submit' , function (e) {
-    e.preventDefault();
+// Gestione Eventi: recupero valore input
+form.addEventListener('submit', function(e) {
+    e.preventDefault(); 
 
-const name = nameInput.value  ;
-const kms = parseInt(kmsInput.value);
-const age = parseInt(ageInput.value) ;
+    const name = nameInput.value;
+    const kms = parseInt(kmsInput.value, 10);
+    const selectedAgeText = ageSelect.options[ageSelect.selectedIndex].text; // Ottieni il testo dell'opzione selezionata
 
-//Elaborazione dati
 
-const basePrice = kms * pricePerKm ;
-let finalPrice = basePrice ;
-
-if (age > 65) {
-    finalPrice *= 0.6;
-} else if (age < 18) {
-    finalPrice *= 0.8;
-}
-
-if (discount) {
     
-    const discount = (finalPrice / 100)* discount;
-    finalPrice = finalPrice - discount ;
-   
-}
-    finalPrice= finalPrice.toFixed(2);
+    const basePrice = kms * pricePerKm;
+    let finalPrice = basePrice;
+
     
-    //Output sulla pagina:
-    paragraphElement.innerHTML= ` Nome e Cognome:</strong> ${name}
-    <strong>Kilometri da percorrere:</strong> ${kms}
-    <strong>Età:</strong> ${age}
-    <strong>Sconto:</strong> ${discount}
-    <strong>Prezzo del biglietto:</strong> € ${finalPrice} `;
-                                
-}
-)
+    if (ageSelect.value === '3') { 
+        finalPrice *= 0.6;
+    } else if (ageSelect.value === '1') {
+        finalPrice *= 0.8;
+    }
+
+ 
+    finalPrice = finalPrice.toFixed(2);
+
+    // Output sulla pagina
+    paragraphElement.innerHTML = `
+        <strong>Nome e Cognome:</strong> ${name}<br>
+        <strong>Kilometri da percorrere:</strong> ${kms}<br>
+        <strong>Età:</strong> ${selectedAgeText}<br>
+        <strong>Prezzo del biglietto:</strong> €${finalPrice}
+    `;
+});
 
 
 
