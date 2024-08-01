142f512ddedf5e007f7f94b8e6049a59d8520f50
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-08-01 17:11:17 +0200

    fix.

--- script.js
+++ script.js
@@ -59,34 +59,3 @@ if (discount) {
 
 
 
-/*const totalElement= document.getElementById('total');
-const discountElement= document.getElementById('discount-message');
-
-const pricePerKm = 0.21;
-
-const priceMessage = 'il tuo biglietto costa €';
-
-let discount = null ;
-const age = parseInt(prompt ('Quanti anni hai?', 30));
-const kms =  parseInt(prompt ('Quantikilometri vuoi percorrere?',100));
-console.log ('kms', kms);
-console.log ('age', age);
-
-const basePrice = kms * pricePerKm ;
-let finalPrice = basePrice ;
-
-if (age > 65) {
-    finalPrice *= 0.6;
-} else if (age < 18) {
-    finalPrice *= 0.8;
-}
-
-if (discount) {
-    const discountElement = document.getElementById ('discount-message');
-    const discountMessage = `Hai ricevuto  uno sconto del ${discount}%`;
-    discountElement.innerHTML = discountMessage ;
-    const discount = (finalPrice / 100)* discount;
-    finalPrice = finalPrice - discount ;
-}
-
-totalElement.innerText = priceMessage + finalPrice.toFixed(2) ; */
